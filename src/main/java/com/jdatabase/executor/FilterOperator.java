5e23449610c88d78c6a961f33250a3e60f5e3866
山泽 <yefei.yf@alibaba-inc.com>
2025-12-23 23:58:00 +0800

    ai

--- src/main/java/com/jdatabase/executor/FilterOperator.java
+++ src/main/java/com/jdatabase/executor/FilterOperator.java
@@ -6,12 +6,19 @@ import com.jdatabase.parser.ast.Expression;
 
 /**
  * 过滤操作符（WHERE子句）
+ * 用于执行SQL查询中的WHERE条件过滤操作
  */
 public class FilterOperator implements Operator {
     private final Operator child;
     private final Expression condition;
     private Tuple nextTuple;
 
+    /**
+     * 构造过滤操作符
+     *
+     * @param child     子操作符
+     * @param condition 过滤条件表达式
+     */
     public FilterOperator(Operator child, Expression condition) {
         this.child = child;
         this.condition = condition;
@@ -42,6 +49,10 @@ public class FilterOperator implements Operator {
         return nextTuple != null;
     }
 
+    /**
+     * 推进到下一个满足条件的元组
+     * 遍历子操作符的元组，找到第一个满足过滤条件的元组
+     */
     private void advance() {
         while (child.hasNext()) {
             Tuple tuple = child.next();
@@ -53,6 +64,14 @@ public class FilterOperator implements Operator {
         nextTuple = null;
     }
 
+    /**
+     * 评估过滤条件
+     * 根据表达式的类型执行相应的条件评估
+     *
+     * @param expr  条件表达式
+     * @param tuple 当前元组
+     * @return 条件是否满足
+     */
     private boolean evaluateCondition(Expression expr, Tuple tuple) {
         if (expr instanceof Expression.BinaryExpression) {
             Expression.BinaryExpression binExpr = (Expression.BinaryExpression) expr;
@@ -73,6 +92,14 @@ public class FilterOperator implements Operator {
         return true;
     }
 
+    /**
+     * 评估表达式
+     * 根据表达式的类型计算其值
+     *
+     * @param expr  表达式
+     * @param tuple 当前元组
+     * @return 表达式的计算结果
+     */
     private Object evaluateExpression(Expression expr, Tuple tuple) {
         if (expr instanceof Expression.Literal) {
             Expression.Literal literal = (Expression.Literal) expr;
@@ -94,6 +121,15 @@ public class FilterOperator implements Operator {
         return null;
     }
 
+    /**
+     * 应用二元操作符
+     * 执行数值类型的加减乘除运算
+     *
+     * @param left  左操作数
+     * @param op    操作符
+     * @param right 右操作数
+     * @return 运算结果
+     */
     private Object applyBinaryOperator(Object left, String op, Object right) {
         if (left == null || right == null) {
             return null;
@@ -120,6 +156,15 @@ public class FilterOperator implements Operator {
         return null;
     }
 
+    /**
+     * 比较值
+     * 根据操作符对两个值进行比较
+     *
+     * @param left  左操作数
+     * @param op    比较操作符
+     * @param right 右操作数
+     * @return 比较结果
+     */
     private boolean compareValues(Object left, String op, Object right) {
         if (op.equals("IS NULL")) {
             return left == null;
@@ -154,14 +199,13 @@ public class FilterOperator implements Operator {
             case ">=":
                 return cmp >= 0;
             case "AND":
-                return (left instanceof Boolean && (Boolean) left) && 
-                       (right instanceof Boolean && (Boolean) right);
+                return (left instanceof Boolean && (Boolean) left) &&
+                        (right instanceof Boolean && (Boolean) right);
             case "OR":
-                return (left instanceof Boolean && (Boolean) left) || 
-                       (right instanceof Boolean && (Boolean) right);
+                return (left instanceof Boolean && (Boolean) left) ||
+                        (right instanceof Boolean && (Boolean) right);
             default:
                 return false;
         }
     }
 }
-
--- src/main/java/com/jdatabase/parser/ast/Expression.java
+++ src/main/java/com/jdatabase/parser/ast/Expression.java
@@ -4,108 +4,221 @@ import java.util.List;
 
 /**
  * 表达式基类
+ * 用于表示SQL查询中的各种表达式节点
  */
 public abstract class Expression {
+
+    /**
+     * 字面量表达式类
+     * 用于表示SQL中的常量值，如数字、字符串等
+     */
     public static class Literal extends Expression {
         private final Object value;
         private final com.jdatabase.common.Types type;
 
+        /**
+         * 构造字面量表达式
+         *
+         * @param value 字面量的值
+         * @param type  字面量的数据类型
+         */
         public Literal(Object value, com.jdatabase.common.Types type) {
             this.value = value;
             this.type = type;
         }
 
+        /**
+         * 获取字面量的值
+         *
+         * @return 字面量的值
+         */
         public Object getValue() {
             return value;
         }
 
+        /**
+         * 获取字面量的数据类型
+         *
+         * @return 字面量的数据类型
+         */
         public com.jdatabase.common.Types getType() {
             return type;
         }
     }
 
+    /**
+     * 列引用表达式类
+     * 用于表示SQL中的列引用，包含表名和列名
+     */
     public static class ColumnReference extends Expression {
         private final String tableName;
         private final String columnName;
 
+        /**
+         * 构造列引用表达式
+         *
+         * @param tableName  表名
+         * @param columnName 列名
+         */
         public ColumnReference(String tableName, String columnName) {
             this.tableName = tableName;
             this.columnName = columnName;
         }
 
+        /**
+         * 获取表名
+         *
+         * @return 表名
+         */
         public String getTableName() {
             return tableName;
         }
 
+        /**
+         * 获取列名
+         *
+         * @return 列名
+         */
         public String getColumnName() {
             return columnName;
         }
     }
 
+    /**
+     * 二元表达式类
+     * 用于表示SQL中的二元操作，如加减乘除、比较操作等
+     */
     public static class BinaryExpression extends Expression {
         private final Expression left;
         private final String operator;
         private final Expression right;
 
+        /**
+         * 构造二元表达式
+         *
+         * @param left     左操作数
+         * @param operator 操作符
+         * @param right    右操作数
+         */
         public BinaryExpression(Expression left, String operator, Expression right) {
             this.left = left;
             this.operator = operator;
             this.right = right;
         }
 
+        /**
+         * 获取左操作数
+         *
+         * @return 左操作数表达式
+         */
         public Expression getLeft() {
             return left;
         }
 
+        /**
+         * 获取操作符
+         *
+         * @return 操作符字符串
+         */
         public String getOperator() {
             return operator;
         }
 
+        /**
+         * 获取右操作数
+         *
+         * @return 右操作数表达式
+         */
         public Expression getRight() {
             return right;
         }
     }
 
+    /**
+     * 一元表达式类
+     * 用于表示SQL中的一元操作，如NOT、负号等
+     */
     public static class UnaryExpression extends Expression {
         private final String operator;
         private final Expression operand;
 
+        /**
+         * 构造一元表达式
+         *
+         * @param operator 操作符
+         * @param operand  操作数
+         */
         public UnaryExpression(String operator, Expression operand) {
             this.operator = operator;
             this.operand = operand;
         }
 
+        /**
+         * 获取操作符
+         *
+         * @return 操作符字符串
+         */
         public String getOperator() {
             return operator;
         }
 
+        /**
+         * 获取操作数
+         *
+         * @return 操作数表达式
+         */
         public Expression getOperand() {
             return operand;
         }
     }
 
+    /**
+     * 函数调用表达式类
+     * 用于表示SQL中的函数调用，如COUNT、SUM等聚合函数
+     */
     public static class FunctionCall extends Expression {
         private final String functionName;
         private final List<Expression> arguments;
         private final boolean distinct;
 
+        /**
+         * 构造函数调用表达式
+         *
+         * @param functionName 函数名
+         * @param arguments    函数参数列表
+         * @param distinct     是否使用DISTINCT关键字
+         */
         public FunctionCall(String functionName, List<Expression> arguments, boolean distinct) {
             this.functionName = functionName;
             this.arguments = arguments;
             this.distinct = distinct;
         }
 
+        /**
+         * 获取函数名
+         *
+         * @return 函数名
+         */
         public String getFunctionName() {
             return functionName;
         }
 
+        /**
+         * 获取函数参数列表
+         *
+         * @return 函数参数表达式列表
+         */
         public List<Expression> getArguments() {
             return arguments;
         }
 
+        /**
+         * 判断是否使用DISTINCT关键字
+         *
+         * @return 是否使用DISTINCT
+         */
         public boolean isDistinct() {
             return distinct;
         }
     }
 }
-
